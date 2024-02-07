c119cb0b1d5142a0c1bd2228565824f3748955ad
Gabriel-Steps <158843321+Gabriel-Steps@users.noreply.github.com>
2024-02-06 23:33:09 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
 let Nome = "Guessero"
-let XP = 3000
-switch(true){
-    case XP < 1001:
+let XP = 6000
+let valores_min = [1001,2001,5001,7001,8001,9001]
+let valores_max = [2000,5000,7000,8000,9000,10000]
+let ranks = ["Bronze","Prata","Ouro","Platina","Ascendente","Imortal"]
+for(let i = 0;i<ranks.length;i++){
+    if(XP < valores_min[0]){
         console.log("O Herói de nome " + Nome + " está no nível de Ferro")
         break
-    case XP >= 1001 && XP <= 2000:
-        console.log("O Herói de nome " + Nome + " está no nível de Bronze")
+    }else if(XP >= valores_min[i] && XP <= valores_max[i]){
+        console.log("O Herói de nome " + Nome + " está no nível de " + ranks[i])
         break
-    case XP >= 2001 && XP <= 5000:
-        console.log("O Herói de nome " + Nome + " está no nível de Prata")
-        break
-    case XP >= 5001 && XP <= 7000:
-        console.log("O Herói de nome " + Nome + " está no nível de Ouro")
-        break
-    case XP >= 7001 && XP <= 8000:
-        console.log("O Herói de nome " + Nome + " está no nível de Platina")
-        break
-    case XP >= 8001 && XP <= 9000:
-        console.log("O Herói de nome " + Nome + " está no nível de Ascendente")
-        break     
-    case XP >= 9001 && XP <= 10000:
-        console.log("O Herói de nome " + Nome + " está no nível de Imortal")
-        break   
-    default:
+    }else if(XP > valores_max[5]){
         console.log("O Herói de nome " + Nome + " está no nível de Radiante")
         break
+    }
 }
